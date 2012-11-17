911157986c978003d61ed06bfdd2f495ea31fc5c
Jared Sealey <jsealey762@gmail.com>
2012-11-16 21:18:58 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartorhot::Application.config.secret_token = 'dcd25836de4a845a9fadd06b4db0f2a0483ef2e4d4b2340daeaad13816da1c29f98fb69b733fbb4801687f5c1954ad74320e43fb489bb54bddf7ed9bef4ee03c'
