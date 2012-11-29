1c4fed1e0af94ab2117bf8defc1442ee09b0995b
Jared Sealey <jsealey762@gmail.com>
2012-11-28 18:24:54 -0800

    add checks for if user is logged in for globals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < ActionController::Base
   before_filter :load_globals
 
   def load_globals
-  	@extended_profile ||= ExtendedProfile.find_by_user_id(current_user.id)
- 	@extended_profile ||= ExtendedProfile.new
- 	@extended_profile.user_id = current_user.id
+  	if user_signed_in?
+  	  @extended_profile ||= ExtendedProfile.find_by_user_id(current_user.id)
+ 	  @extended_profile ||= ExtendedProfile.new
+ 	  @extended_profile.user_id = current_user.id
+ 	end
   end
 
 end
--- app/models/extended_profile.rb
+++ app/models/extended_profile.rb
@@ -2,5 +2,6 @@ class ExtendedProfile < ActiveRecord::Base
   belongs_to :user
   attr_accessible :date_of_birth, :user_id, :avatar, :first_name, :ispremium, :last_name, :phone_number, :references, :sex
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates :first_name, :length => {:in => 1..50}
+  validates :first_name, :length => {:in => 0..50}
+  validates :last_name, :length => {:in => 0..50}
 end
