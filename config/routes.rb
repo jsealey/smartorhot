180d6dcd2658bf7136e7f182401818cefebfc3e1
Jared Sealey <jsealey762@gmail.com>
2012-11-29 17:27:45 -0800

    logo and most attractive

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Smartorhot::Application.routes.draw do
 
     devise_for :users
     resources :photos
+    match "attractive" => "public_pages#attractive"
 	# match "show" => "photos#show",:as => :photo
 	root :to => "public_pages#home"
 end
