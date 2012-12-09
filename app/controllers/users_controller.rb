cb513b3b82e39ef9202b6a5f9a37b23db0a3a3c3
Jared Sealey <jsealey762@gmail.com>
2012-12-08 19:50:44 -0800

    user images on extended profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		@their_extended_profile ||= ExtendedProfile.new
 		@their_extended_profile ||= ExtendedProfile.find_by_user_id(params[:id])
+		@their_extended_profile ||= ExtendedProfile.new
 	end
 	
 end
