4d56b67832ac626a03ee59fbd1e5e680693ccd1d
Pardeep Singh <psingh16@mail.csuchico.edu>
2012-12-08 19:24:51 -0800

    some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :load_globals
+  helper_method :has_voted
 
   def load_globals
   	if user_signed_in?
   	  @extended_profile ||= ExtendedProfile.find_by_user_id(current_user.id)
- 	  @extended_profile ||= ExtendedProfile.new
- 	  @extended_profile.user_id = current_user.id
- 	end
+   	  @extended_profile ||= ExtendedProfile.new
+   	  @extended_profile.user_id = current_user.id
+   	end
+  end
+
+  def has_voted(candidate_id)
+    voted = UserVote.where("candidate_user_id = ? and user_id = ?", candidate_id, current_user.id)
+    if voted
+      return true
+    else
+      return false
+    end
   end
 
 end
