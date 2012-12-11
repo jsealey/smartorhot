56ada74fcb15cc687d99c5d55da9f5ba5d3750f1
Jared Sealey <jsealey762@gmail.com>
2012-12-11 15:40:20 -0800

    fix paperclip image rendering

--- app/models/extended_profile.rb
+++ app/models/extended_profile.rb
@@ -2,7 +2,7 @@ class ExtendedProfile < ActiveRecord::Base
   belongs_to :user
   attr_accessible :date_of_birth, :user_id, :avatar, :first_name, :ispremium, :last_name, :phone_number, :references, :sex, :about
   has_attached_file :avatar, :storage => :dropbox, :dropbox_credentials => "#{Rails.root}/config/dropbox.yml", :styles => { :medium => "300x325#", :thumb => "100x100#" }, :dropbox_options => {:unique_filename => true,:path => proc { |style| "#{style}/#{id}_#{avatar.original_filename}"}}
-
+  validates_attachment_presence :avatar
   validates :first_name, :length => {:in => 0..50}
   validates :last_name, :length => {:in => 0..50}
 end
